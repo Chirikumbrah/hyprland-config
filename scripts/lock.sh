761febceeecda6bee4f3ef7df09e24f1e0190dcc
Chirikumbrah <basslo@protonmail.com>
2023-07-07 18:17:39 +0300

    added element keybinding

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # If idle for 11s, power down the output
-swayidle timeout 11 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' &
+# swayidle timeout 11 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' &
 
+# exec swayidle -w \
+#       timeout 5 'swaylock' \
+#       timeout 5 'hyprctl dispatch dpms off' \
+#       resume 'hyprctl dispatch dpms on' \
+#       before-sleep 'swaylock'
 # Lock screen immediately
-swaylock
+# swaylock
 
 # Screen unlocked: terminate swayidle and clean up PID
 kill -TERM $!

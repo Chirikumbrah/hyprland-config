950fed860274a3b525f97587029fd3203b8b0c80
Chirikumbrah <basslo@protonmail.com>
2023-08-25 12:02:41 +0300

    added udiskie to autostart

--- scripts/open-app.sh
+++ scripts/open-app.sh
@@ -4,7 +4,7 @@ APP=$1
 CLASS=$2
 WORKSPACE=$3
 
-if $(hyprctl clients | rg -q "class: $CLASS"); then
+if eval "$(hyprctl clients | rg -q "class: $CLASS")"; then
 	hyprctl dispatch workspace "$WORKSPACE" 
 else
 	hyprctl dispatch exec "$APP"

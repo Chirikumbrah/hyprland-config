9427308cf9f9f7d6feb2a46cfa9f946e84b72bde
Chirikumbrah <basslo@protonmail.com>
2023-09-25 12:04:17 +0300

    changed flameshot to slurp

--- scripts/take-screenshot.sh
+++ scripts/take-screenshot.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-export XDG_CURRENT_DESKTOP=sway
-
 case $1 in
-    --full)    flameshot screen ;;
-    --partial) flameshot gui ;;
+--flameshot)
+    case $2 in
+    --full) XDG_CURRENT_DESKTOP=sway flameshot screen ;;
+    --partial) XDG_CURRENT_DESKTOP=sway flameshot gui ;;
+    esac
+    ;;
+--slurp) grim -g "$(slurp)" - | swappy -f - ;;
 esac

fe0755b9b1a3715eb96b0c59a242c8958dc52a8d
Chirikumbrah <basslo@protonmail.com>
2023-09-29 11:21:53 +0300

    moved back to flameshot

--- scripts/take-screenshot.sh
+++ scripts/take-screenshot.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
 case $1 in
---flameshot)
-    case $2 in
-    --full) XDG_CURRENT_DESKTOP=sway flameshot screen ;;
-    --partial) XDG_CURRENT_DESKTOP=sway flameshot gui ;;
-    esac
-    ;;
+--flameshot) flameshot gui ;;
 --slurp) grim -g "$(slurp)" - | swappy -f - ;;
 esac
